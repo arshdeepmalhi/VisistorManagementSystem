7fa01e88e21e7b78e639d08a937e4b38a908e534
Admin <Admin@LAPTOP-798FHRV5>
2021-03-11 13:11:07 +1300

    Add project files.

---
+++ Data/VisistorManagementSystemContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VisistorManagementSystem.Models;
+
+namespace VisistorManagementSystem.Data
+{
+    public class VisistorManagementSystemContext : DbContext
+    {
+        public VisistorManagementSystemContext (DbContextOptions<VisistorManagementSystemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VisistorManagementSystem.Models.Visitors> Visitors { get; set; }
+
+        public DbSet<VisistorManagementSystem.Models.StaffNames> StaffNames { get; set; }
+    }
+}
---
+++ Data/VisitorDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VisistorManagementSystem.Models;
+
+namespace VisistorManagementSystem.Data
+{
+    public class VisitorDbContext : IdentityDbContext
+    {
+        // Here we add in all the tables we are using.
+        public DbSet<StaffNames> StaffNames { get; set; }
+        public DbSet<Visitors> Visitor { get; set; }
+
+        // We need 2 constructors, one is empty, and the other injects in  DbContextOptions
+        public VisitorDbContext(DbContextOptions<VisitorDbContext> options)
+            : base(options)
+        {
+        }
+        public VisitorDbContext()
+        {
+
+        }
+    }
+}
